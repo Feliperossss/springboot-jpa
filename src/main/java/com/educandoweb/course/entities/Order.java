e3efc2cb3584ffe49865f06f3ae6845f30129362
felipe ross <kamigamink@gmail.com>
2026-02-13 15:16:21 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -10,13 +10,16 @@ import com.educandoweb.course.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -37,6 +40,9 @@ public class Order  implements Serializable{
 
 	@OneToMany(mappedBy = "id.order")
 	private  Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade= CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		super();
@@ -79,6 +85,18 @@ public class Order  implements Serializable{
 	}
 	
 	
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+
 
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
